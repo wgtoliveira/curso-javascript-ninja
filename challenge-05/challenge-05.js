28e0c899e6260500b67fdb0449cfd0f24f520690
wgtoliveira <wgtoliveiravendas@gmail.com>
2019-11-17 12:48:20 -0300

    update

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,7 +2,7 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-var myarray = [1, "Bahia", "1", true, false];
+var myarray = [1, "Bahia", "1", true, false]
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.

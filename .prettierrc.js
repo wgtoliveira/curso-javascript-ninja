9626fa8b95f6b78be15f275c05b52ebce1d6cee0
wgtoliveira <wgtoliveiravendas@gmail.com>
2019-11-18 15:06:36 -0300

    Conclusão challenge-06

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
+  bracketSpacing: true,
+}
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -1,51 +1,73 @@
 /*
-Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer
-para começar o desafio.
-Declare uma variável chamada `championship` que receberá o nome do campeonato,
-e imprima o nome desse campeonato no console.
-*/
-// ?
+ *Vamos falar um pouco sobre "Futebol". Escolha um campeonato estadual qualquer
+ *para começar o desafio.
+ *Declare uma variável chamada `championship` que receberá o nome do campeonato,
+ *e imprima o nome desse campeonato no console.
+ */
+var championship = 'Campeonato Brasileiro'
+console.log(championship)
 
 /*
-Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
-Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
-estar na ordem em que eles aparecem na tabela no momento da solução desse
-desafio.
-*/
-// ?
+ *Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
+ *Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
+ *estar na ordem em que eles aparecem na tabela no momento da solução desse
+ *desafio.
+ */
+var teams = ['Flamengo', 'Palmeiras', 'Santos', 'Grêmio', 'Atletico-Pr']
 
-console.log( 'Times que estão participando do campeonato:', teams );
+console.log('Times que estão participando do campeonato:', teams)
+
+/* o valor pass
+ *Crie uma função chamada `showTeamPosition` com as seguintes características:
+ *    - A função deve receber um número por parâmetro;
+ *    - A função deve retornar a frase:
+ *    "O time que está em [POSIÇÃO]º lugar é o [NOME DO TIME].";
+ *    - Onde [POSIÇÃO] é a do por parâmetro e [NOME DO TIME] é o time
+ *    que está nessa posição no array criado acima com os nomes dos times.
+ *    --------------
+ *    Dica: lembre-se que arrays começam no índice zero, então a posição passada
+ *    deve ser sempre um número a mais que o índice do array ;)
+ *    --------------
+ *    - A função só deve retornar a frase acima somente se o time estiver entre
+ *    os 5 primeiros.
+ *    - Se não houver time para a posição passada, deve retornar a mensagem:
+ *    "Não temos a informação do time que está nessa posição."
+ */
+function showTeamPosition(num) {
+  if (num === 1) {
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[0]
+  } else if (num === 2) {
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[1]
+  } else if (num === 3) {
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[2]
+  } else if (num === 4) {
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[3]
+  } else if (num === 5) {
+    return 'O time que está em ' + num + 'º lugar é o ' + teams[4]
+  } else {
+    return 'Não temos a informação do time que está nessa posição.'
+  }
+}
+console.log(showTeamPosition(1))
+console.log(showTeamPosition(2))
+console.log(showTeamPosition(3))
+console.log(showTeamPosition(4))
+console.log(showTeamPosition(5))
+console.log(showTeamPosition(6))
 
 /*
-Crie uma função chamada `showTeamPosition` com as seguintes características:
-    - A função deve receber um número por parâmetro;
-    - A função deve retornar a frase:
-    "O time que está em [POSIÇÃO]º lugar é o [NOME DO TIME].";
-    - Onde [POSIÇÃO] é o valor passado por parâmetro e [NOME DO TIME] é o time
-    que está nessa posição no array criado acima com os nomes dos times.
-    --------------
-    Dica: lembre-se que arrays começam no índice zero, então a posição passada
-    deve ser sempre um número a mais que o índice do array ;)
-    --------------
-    - A função só deve retornar a frase acima somente se o time estiver entre
-    os 5 primeiros.
-    - Se não houver time para a posição passada, deve retornar a mensagem:
-    "Não temos a informação do time que está nessa posição."
-*/
-// ?
-
-/*
-Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
-função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
-*/
-// ?
-
+ *Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
+ *função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
+ */
+//?
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
-
+var num = 20
+while (num <= 30) {
+  console.log(num++)
+}
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
     - A função recebe uma cor por parâmetro, do tipo string. Exemplo: "red";
@@ -58,9 +80,42 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
-
-/*
-Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
-*/
-// ?
+function convertToHex(cor) {
+  switch (cor) {
+    case 'red':
+      console.log('O hexadecimal para a cor ' + cor + ' é #FF0000.')
+      break
+    case 'green':
+      console.log('O hexadecimal para a cor ' + cor + ' é #008000.')
+      break
+    case 'blue':
+      console.log('O hexadecimal para a cor ' + cor + ' é #0000FF.')
+      break
+    case 'black':
+      console.log('O hexadecimal para a cor ' + cor + ' é #000000.')
+      break
+    case 'white':
+      console.log('O hexadecimal para a cor ' + cor + ' é #FFFFFFF.')
+      break
+    case 'yellow':
+      console.log('O hexadecimal para a cor ' + cor + ' é #FFFF00.')
+      break
+    case 'orange':
+      console.log('O hexadecimal para a cor ' + cor + ' é #FFA500.')
+      break
+    case 'purple':
+      console.log('O hexadecimal para a cor ' + cor + ' é #800080.')
+      break
+    default:
+      console.log('Não temos o equivalente hexadecimal para ' + cor)
+  }
+}
+// Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
+convertToHex('green')
+convertToHex('purple')
+convertToHex('black')
+convertToHex('red')
+convertToHex('yellow')
+convertToHex('white')
+convertToHex('blue')
+convertToHex('orange')

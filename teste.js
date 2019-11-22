9c7ac6c9efb581861e99f949eba8c08aac1926bc
wgtoliveira <wgtoliveiravendas@gmail.com>
2019-11-22 11:37:44 -0300

    criação arquivo test

--- teste.js
+++ teste.js
@@ -1,7 +1,13 @@
-let name = 'Washington, Olá Mundo !!! '
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-function myFunction(x) {
-  return x
-}
+const isEven = x => x % 2 == 0
 
-console.log(myFunction(name))
+const myMap = numbers.map(isEven)
+
+console.log('every: ' + numbers.every(isEven))
+console.log('sme' + numbers.some(isEven))
+console.table('myMap: ' + myMap)
+console.table(numbers.forEach(x => console.log(x % 2 === 0)))
+const evenNumbers = console.log(numbers.filter(isEven))
+const myReduce = numbers.reduce((previous, current) => previous + current)
+console.log(myReduce)
